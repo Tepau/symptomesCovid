da630c5482b6a97d1f62cee58c69b6db0f2c6dd7
malaury <malaury.lemaitresalmon@gmail.com>
2020-05-24 12:23:09 +0200

    add procfile

--- ephad/settings.py
+++ ephad/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*^c1*h-tzw23m6uc15m(4anhnva88s7ui0$ki^kiu66(w7)oxf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['symptomes.herokuapp.com']
 
 
 # Application definition
